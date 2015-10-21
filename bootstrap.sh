fe06d942959b9617307e9133e8d6e7217f55f32f
Daniel Rivas <ers.daniel+dev@gmail.com>
2015-10-20 23:20:03 -0500

    Added a Vagrantfile provision for installing Haskell Platform

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+haskellPlatform=https://haskell.org/platform/download/7.10.2/haskell-platform-7.10.2-a-unknown-linux-deb7.tar.gz
+
+# PACKAGES
+apt-get update
+apt-get install -y git
+
+# LIBRARIES NEEDED BY HASKELL APPS
+apt-get install -y libgmp-dev
+
+# HASKELL PLATFORM
+mkdir haskell-platform
+cd haskell-platform
+
+wget -O haskell-platform.tar.gz $haskellPlatform
+tar -xzvf haskell-platform.tar.gz
+./install-haskell-platform.sh
+
+cd ../
+rm -rf haskell-platform
